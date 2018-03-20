4e98a0cd35a223bde1c3c31bbff4c3f7e2d347ba
Duy <tranleduy1233@gmail.com>
2018-03-20 14:11:12 +0700

    update test case

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/expression/IComplexImpl.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/expression/IComplexImpl.java
@@ -7,4 +7,6 @@ import org.matheclipse.core.interfaces.IComplex;
  * Created by Duy on 2/20/2018.
  */
 public abstract class IComplexImpl extends IBigNumberImpl implements IComplex {
+    @Override
+    public abstract IComplex inverse();
 }
