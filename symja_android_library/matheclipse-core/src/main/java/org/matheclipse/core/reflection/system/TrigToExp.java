63f920cf8fb32310e2b3052f2d55f36c3eda595f
Duy <tranleduy1233@gmail.com>
2018-03-30 17:44:22 +0700

    ExprPreprocessor for generating Java source code from Symja expressions.
    Convert Symja expressions line comments opened by // [$ ... $] and
    closed by // $$  into Java source code.
    See TrigToExp example

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/TrigToExp.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/TrigToExp.java
@@ -3,6 +3,7 @@ package org.matheclipse.core.reflection.system;
 import org.matheclipse.core.eval.EvalEngine;
 import org.matheclipse.core.eval.exception.Validate;
 import org.matheclipse.core.eval.interfaces.AbstractEvaluator;
+import org.matheclipse.core.expression.F;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.ISymbol;
@@ -11,44 +12,29 @@ import org.matheclipse.core.patternmatching.Matcher;
 import static org.matheclipse.core.expression.F.ArcCos;
 import static org.matheclipse.core.expression.F.ArcSin;
 import static org.matheclipse.core.expression.F.ArcTan;
-import static org.matheclipse.core.expression.F.C1;
-import static org.matheclipse.core.expression.F.C1D2;
-import static org.matheclipse.core.expression.F.C2;
-import static org.matheclipse.core.expression.F.CI;
-import static org.matheclipse.core.expression.F.CN1;
-import static org.matheclipse.core.expression.F.CNI;
 import static org.matheclipse.core.expression.F.Cos;
 import static org.matheclipse.core.expression.F.Cosh;
 import static org.matheclipse.core.expression.F.Coth;
 import static org.matheclipse.core.expression.F.Csch;
-import static org.matheclipse.core.expression.F.E;
-import static org.matheclipse.core.expression.F.Log;
-import static org.matheclipse.core.expression.F.Pi;
-import static org.matheclipse.core.expression.F.Plus;
-import static org.matheclipse.core.expression.F.Power;
 import static org.matheclipse.core.expression.F.Sech;
 import static org.matheclipse.core.expression.F.Sin;
 import static org.matheclipse.core.expression.F.Sinh;
-import static org.matheclipse.core.expression.F.Sqr;
-import static org.matheclipse.core.expression.F.Sqrt;
-import static org.matheclipse.core.expression.F.Subtract;
 import static org.matheclipse.core.expression.F.Tan;
 import static org.matheclipse.core.expression.F.Tanh;
-import static org.matheclipse.core.expression.F.Times;
 import static org.matheclipse.core.expression.F.x_;
 
 /**
  * <pre>
  * TrigToExp(expr)
  * </pre>
- * 
+ *
  * <blockquote>
  * <p>
  * converts trigonometric functions in <code>expr</code> to exponentials.
  * </p>
  * </blockquote>
  * <h3>Examples</h3>
- * 
+ *
  * <pre>
  * &gt;&gt; TrigToExp(Cos(x))
  * 1/2*E^(I*x)+1/2*E^(-I*x)
@@ -58,46 +44,48 @@ public class TrigToExp extends AbstractEvaluator {
 
 	final static Matcher MATCHER = new Matcher(EvalEngine.get());
 	static {
-		// I/(2*E^(I*x))-1/2*I*E^(I*x)
 		MATCHER.caseOf(Sin(x_), //
-				x -> Subtract(Times(C1D2, CI, Power(E, Times(CNI, x))), Times(C1D2, CI, Power(E, Times(CI, x)))));
-		// 1/(2*E^(I*x))+E^(I*x)/2
+				x -> // [$ I/(2*E^(I*x))-1/2*I*E^(I*x) $]
+						F.Plus(F.Times(F.CN1D2, F.CI, F.Exp(F.Times(F.CI, x))),
+								F.Times(F.CI, F.Power(F.Times(F.C2, F.Exp(F.Times(F.CI, x))), -1)))); // $$);
 		MATCHER.caseOf(Cos(x_), //
-				x -> Plus(Times(C1D2, Power(E, Times(CNI, x))), Times(C1D2, Power(E, Times(CI, x)))));
-		// (I*(E^(-I*x)-E^(I*x)))/(E^(-I*x)+E^(I*x))
+				x -> // [$ 1/(2*E^(I*x))+E^(I*x)/2 $]
+						F.Plus(F.Power(F.Times(F.C2, F.Exp(F.Times(F.CI, x))), -1), F.Times(F.C1D2, F.Exp(F.Times(F.CI, x))))); // $$);
 		MATCHER.caseOf(Tan(x_), //
-				x -> Times(CI, Subtract(Power(E, Times(CNI, x)), Power(E, Times(CI, x))),
-				Power(Plus(Power(E, Times(CNI, x)), Power(E, Times(CI, x))), CN1)));
-		// -I*Log(I*x+Sqrt(1-x^2))
+				x -> // [$ (I*(E^(-I*x)-E^(I*x)))/(E^(-I*x)+E^(I*x)) $]
+						F.Times(F.CI, F.Plus(F.Exp(F.Times(F.CNI, x)), F.Negate(F.Exp(F.Times(F.CI, x)))),
+								F.Power(F.Plus(F.Exp(F.Times(F.CNI, x)), F.Exp(F.Times(F.CI, x))), -1))); // $$);
 		MATCHER.caseOf(ArcSin(x_), //
-				x -> Times(CNI, Log(Plus(Sqrt(Subtract(C1, Sqr(x))), Times(CI, x)))));
-		// Pi/2+I*Log(I*x+Sqrt(1-x^2))
+				x -> // [$ -I*Log(I*x+Sqrt(1-x^2)) $]
+						F.Times(F.CNI, F.Log(F.Plus(F.Times(F.CI, x), F.Sqrt(F.Plus(F.C1, F.Negate(F.Sqr(x)))))))); // $$);
 		MATCHER.caseOf(ArcCos(x_), //
-				x -> Plus(Times(C1D2, Pi), Times(CI, Log(Plus(Sqrt(Subtract(C1, Sqr(x))), Times(CI, x))))));
-		// 1/2*I*Log(1-I*x)-1/2*I*Log(1+I*x)
+				x -> // [$ Pi/2+I*Log(I*x+Sqrt(1-x^2)) $]
+						F.Plus(F.Times(F.C1D2, F.Pi),
+								F.Times(F.CI, F.Log(F.Plus(F.Times(F.CI, x), F.Sqrt(F.Plus(F.C1, F.Negate(F.Sqr(x))))))))); // $$);
 		MATCHER.caseOf(ArcTan(x_), //
-				x -> Subtract(Times(C1D2, CI, Log(Plus(C1, Times(CNI, x)))),
-						Times(C1D2, CI, Log(Plus(C1, Times(CI, x))))));
-		// (E^x+E^(-x))/2
+				x -> // [$ 1/2*I*Log(1-I*x)-1/2*I*Log(1+I*x) $]
+						F.Plus(F.Times(F.C1D2, F.CI, F.Log(F.Plus(F.C1, F.Times(F.CNI, x)))),
+								F.Times(F.CN1D2, F.CI, F.Log(F.Plus(F.C1, F.Times(F.CI, x)))))); // $$);
 		MATCHER.caseOf(Cosh(x_), //
-				x -> Times(C1D2, Plus(Power(E, x), Power(E, Times(CN1, x)))));
-		// 2/(E^x-E^(-x))
+				x -> // [$ (E^x+E^(-x))/2 $]
+						F.Times(F.C1D2, F.Plus(F.Exp(x), F.Exp(F.Negate(x))))); // $$);
 		MATCHER.caseOf(Csch(x_), //
-				x -> Times(C2, Power(Plus(Power(E, x), Times(CN1, Power(E, Times(CN1, x)))), CN1)));
-		// ((E^(-x))+E^x)/((-E^(-x))+E^x)
+				x -> // [$ 2/(E^x-E^(-x)) $]
+						F.Times(F.C2, F.Power(F.Plus(F.Negate(F.Exp(F.Negate(x))), F.Exp(x)), -1))); // $$);
 		MATCHER.caseOf(Coth(x_), //
-				x -> Times(Plus(Power(E, x), Power(E, Times(CN1, x))),
-				Power(Plus(Power(E, x), Times(CN1, Power(E, Times(CN1, x)))), CN1)));
-		// 2/(E^x+E^(-x))
+				x -> // [$ ((E^(-x))+E^x)/((-E^(-x))+E^x) $]
+						F.Times(F.Plus(F.Exp(F.Negate(x)), F.Exp(x)),
+								F.Power(F.Plus(F.Negate(F.Exp(F.Negate(x))), F.Exp(x)), -1))); // $$);
 		MATCHER.caseOf(Sech(x_), //
-				x -> Times(C2, Power(Plus(Power(E, x), Power(E, Times(CN1, x))), CN1)));
-		// (E^x-E^(-x))/2
+				x -> // [$ 2/(E^x+E^(-x)) $]
+						F.Times(F.C2, F.Power(F.Plus(F.Exp(x), F.Exp(F.Negate(x))), -1))); // $$);
 		MATCHER.caseOf(Sinh(x_), //
-				x -> Times(C1D2, Plus(Power(E, x), Times(CN1, Power(E, Times(CN1, x))))));
-		// ((-E^(-x))+E^x)/((E^(-x))+E^x)
+				x -> // [$ (E^x-E^(-x))/2 $]
+						F.Times(F.C1D2, F.Plus(F.Negate(F.Exp(F.Negate(x))), F.Exp(x)))); // $$);
 		MATCHER.caseOf(Tanh(x_), //
-				x -> Times(Plus(Times(CN1, Power(E, Times(CN1, x))), Power(E, x)),
-				Power(Plus(Power(E, Times(CN1, x)), Power(E, x)), CN1)));
+				x -> // [$ ((-E^(-x))+E^x)/((E^(-x))+E^x) $]
+						F.Times(F.Plus(F.Negate(F.Exp(F.Negate(x))), F.Exp(x)),
+								F.Power(F.Plus(F.Exp(F.Negate(x)), F.Exp(x)), -1))); // $$);
 	}
 
 	public TrigToExp() {
@@ -105,7 +93,7 @@ public class TrigToExp extends AbstractEvaluator {
 
 	/**
 	 * Exponential definitions for trigonometric functions
-	 * 
+	 *
 	 * See <a href= "http://en.wikipedia.org/wiki/List_of_trigonometric_identities#Exponential_definitions"> List of
 	 * trigonometric identities - Exponential definitions</a>,<br/>
 	 * <a href="http://en.wikipedia.org/wiki/Hyperbolic_function">Hyperbolic function</a>
