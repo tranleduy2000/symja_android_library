a597a2d73e2e91771568436da3104641d83fdafb
Duy <tranleduy1233@gmail.com>
2018-05-11 20:54:13 +0700

    markdown

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/PolynomialFunctions.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/PolynomialFunctions.java
@@ -1418,6 +1418,29 @@ public class PolynomialFunctions {
 
 	}
 
+	/**
+	 * <pre>
+	 * ChebyshevT(n, x)
+	 * </pre>
+	 *
+	 * <blockquote>
+	 * <p>
+	 * returns the Chebyshev polynomial of the first kind <code>T_n(x)</code>.
+	 * </p>
+	 * </blockquote>
+	 * <p>
+	 * See:<br />
+	 * </p>
+	 * <ul>
+	 * <li><a href="https://en.wikipedia.org/wiki/Chebyshev_polynomials">Wikipedia - Chebyshev polynomials</a></li>
+	 * </ul>
+	 * <h3>Examples</h3>
+	 *
+	 * <pre>
+	 * &gt;&gt; ChebyshevT(8, x)
+	 * 1-32*x^2+160*x^4-256*x^6+128*x^8
+	 * </pre>
+	 */
 	final private static class ChebyshevT extends AbstractFunctionEvaluator {
 
 		@Override
@@ -1448,6 +1471,29 @@ public class PolynomialFunctions {
 		}
 	}
 
+	/**
+	 * <pre>
+	 * ChebyshevU(n, x)
+	 * </pre>
+	 *
+	 * <blockquote>
+	 * <p>
+	 * returns the Chebyshev polynomial of the second kind <code>U_n(x)</code>.
+	 * </p>
+	 * </blockquote>
+	 * <p>
+	 * See:<br />
+	 * </p>
+	 * <ul>
+	 * <li><a href="https://en.wikipedia.org/wiki/Chebyshev_polynomials">Wikipedia - Chebyshev polynomials</a></li>
+	 * </ul>
+	 * <h3>Examples</h3>
+	 *
+	 * <pre>
+	 * &gt;&gt; ChebyshevU(8, x)
+	 * 1-40*x^2+240*x^4-448*x^6+256*x^8
+	 * </pre>
+	 */
 	final private static class ChebyshevU extends AbstractFunctionEvaluator {
 
 		@Override
@@ -1486,6 +1532,28 @@ public class PolynomialFunctions {
 		}
 	}
 
+	/**
+	 * <pre>
+	 * BellY(n, k, {x1, x2, ... , xN})
+	 * </pre>
+	 *
+	 * <blockquote>
+	 * <p>
+	 * the second kind of Bell polynomials (incomplete Bell polynomials).
+	 * </p>
+	 * </blockquote>
+	 * <p>
+	 * See:<br />
+	 * </p>
+	 * <ul>
+	 * <li><a href="https://en.wikipedia.org/wiki/Bell_polynomials">Wikipedia - Bell polynomials</a></li>
+	 * </ul>
+	 *
+	 * <pre>
+	 * &gt;&gt; BellY(6, 2, {x1, x2, x3, x4, x5})
+	 * 10*x3^2+15*x2*x4+6*x1*x5
+	 * </pre>
+	 */
 	final private static class BellY extends AbstractFunctionEvaluator {
 
 		@Override
@@ -1541,6 +1609,32 @@ public class PolynomialFunctions {
 		}
 	}
 
+	/**
+	 * <pre>
+	 * HermiteH(n, x)
+	 * </pre>
+	 *
+	 * <blockquote>
+	 * <p>
+	 * returns the Hermite polynomial <code>H_n(x)</code>.
+	 * </p>
+	 * </blockquote>
+	 * <p>
+	 * See:<br />
+	 * </p>
+	 * <ul>
+	 * <li><a href="https://en.wikipedia.org/wiki/Hermite_polynomials">Wikipedia - Hermite polynomials</a></li>
+	 * </ul>
+	 * <h3>Examples</h3>
+	 *
+	 * <pre>
+	 * &gt;&gt; HermiteH(8, x)
+	 * 1680-13440*x^2+13440*x^4-3584*x^6+256*x^8
+	 *
+	 * &gt;&gt; HermiteH(3, 1 + I)
+	 * -28+I*4
+	 * </pre>
+	 */
 	final private static class HermiteH extends AbstractFunctionEvaluator {
 
 		@Override
@@ -1556,6 +1650,29 @@ public class PolynomialFunctions {
 
 	}
 
+	/**
+	 * <pre>
+	 * LaguerreL(n, x)
+	 * </pre>
+	 *
+	 * <blockquote>
+	 * <p>
+	 * returns the Laguerre polynomial <code>L_n(x)</code>.
+	 * </p>
+	 * </blockquote>
+	 * <p>
+	 * See:<br />
+	 * </p>
+	 * <ul>
+	 * <li><a href="https://en.wikipedia.org/wiki/Laguerre_polynomials">Wikipedia - Laguerre polynomials</a></li>
+	 * </ul>
+	 * <h3>Examples</h3>
+	 *
+	 * <pre>
+	 * &gt;&gt; LaguerreL(8, x)
+	 * 1-8*x+14*x^2-28/3*x^3+35/12*x^4-7/15*x^5+7/180*x^6-x^7/630+x^8/40320
+	 * </pre>
+	 */
 	final private static class LaguerreL extends AbstractFunctionEvaluator {
 
 		@Override
@@ -1573,6 +1690,29 @@ public class PolynomialFunctions {
 
 	}
 
+	/**
+	 * <pre>
+	 * LegendreP(n, x)
+	 * </pre>
+	 *
+	 * <blockquote>
+	 * <p>
+	 * returns the Legendre polynomial <code>P_n(x)</code>.
+	 * </p>
+	 * </blockquote>
+	 * <p>
+	 * See:<br />
+	 * </p>
+	 * <ul>
+	 * <li><a href="https://en.wikipedia.org/wiki/Legendre_polynomials">Wikipedia - Legendre polynomials</a></li>
+	 * </ul>
+	 * <h3>Examples</h3>
+	 *
+	 * <pre>
+	 * &gt;&gt; LegendreP(4, x)
+	 * 3/8-15/4*x^2+35/8*x^4
+	 * </pre>
+	 */
 	final private static class LegendreP extends AbstractFunctionEvaluator implements LegendrePRules {
 
 		@Override
@@ -1593,6 +1733,29 @@ public class PolynomialFunctions {
 
 	}
 
+	/**
+	 * <pre>
+	 * LegendreQ(n, x)
+	 * </pre>
+	 *
+	 * <blockquote>
+	 * <p>
+	 * returns the Legendre functions of the second kind <code>Q_n(x)</code>.
+	 * </p>
+	 * </blockquote>
+	 * <p>
+	 * See:<br />
+	 * </p>
+	 * <ul>
+	 * <li><a href="https://en.wikipedia.org/wiki/Legendre_polynomials">Wikipedia - Legendre polynomials</a></li>
+	 * </ul>
+	 * <h3>Examples</h3>
+	 *
+	 * <pre>
+	 * &gt;&gt; Expand(LegendreQ(4,z))
+	 * 55/24*z-35/8*z^3-3/16*Log(1-z)+15/8*z^2*Log(1-z)-35/16*z^4*Log(1-z)+3/16*Log(1+z)-15/8*z^2*Log(1+z)+35/16*z^4*Log(1+z)
+	 * </pre>
+	 */
 	final static class LegendreQ extends AbstractFunctionEvaluator implements LegendreQRules {
 
 		@Override
