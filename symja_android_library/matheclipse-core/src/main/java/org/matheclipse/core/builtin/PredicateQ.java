8f82dedf780f780dd59c890af294c0b74b44cf2d
Duy <tranleduy1233@gmail.com>
2018-06-14 22:35:22 +0700

    SquareMatrixQ() function / more markdown for predicates axexlck 22-May-18 2:23 AM

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/PredicateQ.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/PredicateQ.java
@@ -246,7 +246,12 @@ public class PredicateQ {
 			Predicate<IExpr> pred = null;
 			if ((ast.size() >= 4)) {
 				final IExpr arg3 = engine.evaluate(ast.arg3());
-				pred = x -> engine.evalTrue(F.unaryAST1(arg3, x));
+				pred = new Predicate<IExpr>() {
+					@Override
+					public boolean test(IExpr x) {
+						return engine.evalTrue(F.unaryAST1(arg3, x));
+					}
+				};
 			}
 			int depth = determineDepth(arg1, 0, pred);
 			if (depth >= 0) {
@@ -875,9 +880,12 @@ public class PredicateQ {
 				temp.append(F.Slot1);
 				IAST matrix = (IAST) arg1;
 				for (int i = 1; i < dims[0]; i++) {
-					if (!((IAST) matrix.get(i)).forAll(x -> {
-						temp.set(1, x);
-						return engine.evalTrue(temp);
+					if (!((IAST) matrix.get(i)).forAll(new com.duy.lambda.Predicate<IExpr>() {
+						@Override
+						public boolean test(IExpr x) {
+							temp.set(1, x);
+							return engine.evalTrue(temp);
+						}
 					})) {
 						return F.False;
 					}
@@ -1626,9 +1634,12 @@ public class PredicateQ {
 				temp.append(F.Slot1);
 
 				IAST vector = (IAST) arg1;
-				if (!vector.forAll(x -> {
-					temp.set(1, x);
-					return engine.evalTrue(temp);
+				if (!vector.forAll(new com.duy.lambda.Predicate<IExpr>() {
+					@Override
+					public boolean test(IExpr x) {
+						temp.set(1, x);
+						return engine.evalTrue(temp);
+					}
 				})) {
 					return F.False;
 				}
