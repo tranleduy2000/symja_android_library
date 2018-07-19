73612c9ddd1768ad017accd430e0f4ada9dd34f5
Duy <tranleduy1233@gmail.com>
2018-07-19 22:14:41 +0700

    Hypergeometric0F1() function axexlck 19-Jul-18 5:06 AM

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/HypergeometricFunctions.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/HypergeometricFunctions.java
@@ -14,6 +14,7 @@ import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.IInteger;
 import org.matheclipse.core.interfaces.ISignedNumber;
 import org.matheclipse.core.interfaces.ISymbol;
+import org.matheclipse.core.reflection.system.rules.Hypergeometric0F1Rules;
 import org.matheclipse.core.reflection.system.rules.Hypergeometric2F1Rules;
 
 import static org.matheclipse.core.expression.F.C1D2;
