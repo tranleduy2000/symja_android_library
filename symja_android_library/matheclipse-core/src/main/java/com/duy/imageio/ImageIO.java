a41c153e5f80beca207ea83dd92e04fa7ae64138
Duy <tranleduy1233@gmail.com>
2018-07-27 23:12:15 +0700

    Ported ImageIO

--- symja_android_library/matheclipse-core/src/main/java/com/duy/imageio/ImageIO.java
+++ symja_android_library/matheclipse-core/src/main/java/com/duy/imageio/ImageIO.java
@@ -1,4 +1,30 @@
 package com.duy.imageio;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ImageIO {
+    public static Bitmap read(InputStream inputStream) {
+        return BitmapFactory.decodeStream(inputStream);
+    }
+
+    public static void write(Bitmap bitmap, String ext, File file) throws FileNotFoundException {
+        FileOutputStream output = new FileOutputStream(file);
+        Bitmap.CompressFormat compressFormat = Bitmap.CompressFormat.valueOf(ext);
+        bitmap.compress(compressFormat, 100, output);
+    }
+
+    public static Bitmap read(File file) throws IOException {
+        FileInputStream inputStream = new FileInputStream(file);
+        Bitmap bitmap = read(inputStream);
+        inputStream.close();
+        return bitmap;
+    }
 }
--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Export.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Export.java
@@ -20,105 +20,104 @@ import java.nio.charset.Charset;
 
 import ch.ethz.idsc.tensor.io.Extension;
 import ch.ethz.idsc.tensor.io.Filename;
+import ch.ethz.idsc.tensor.io.ImageFormat;
 
 /**
  * Export some data from file system.
- *
  */
 public class Export extends AbstractEvaluator {
 
-	public Export() {
-	}
+    public Export() {
+    }
 
-	@Override
-	public IExpr evaluate(final IAST ast, EvalEngine engine) {
-		if (Config.isFileSystemEnabled(engine)) {
-			Validate.checkRange(ast, 3, 4);
+    /**
+     * See the documentation of {@link CsvFormat}, {@link ImageFormat}, {@link MatlabExport}, and {@link ObjectFormat}
+     * for information on how tensors are encoded in the respective format.
+     *
+     * @param file   destination
+     * @param tensor
+     * @throws IOException
+     */
+    public static void of(File file, IAST tensor) throws IOException {
+        Filename filename = new Filename(file);
+        // if (filename.hasExtension("csv"))
+        // Files.write(file.toPath(), (Iterable<String>) CsvFormat.of(tensor)::iterator);
+        // else
+        Extension extension = filename.extension();
+        if (extension.equals(Extension.JPG))
+            ImageIO.write(ImageFormat.jpg(tensor), "jpg", file);
+        // else if (filename.hasExtension("m"))
+        // Files.write(file.toPath(), (Iterable<String>) MatlabExport.of(tensor)::iterator);
+        if (extension.equals(Extension.PNG))
+            ImageIO.write(ImageFormat.of(tensor), "png", file);
+            // else if (filename.hasExtension("tensor"))
+            // object(file, tensor);
+        else
+            throw new RuntimeException(file.toString());
+    }
 
-			if (!(ast.arg1() instanceof IStringX)) {
-				throw new WrongNumberOfArguments(ast, 1, ast.argSize());
-			}
-			String format = "Data";
-			if (ast.size() == 4) {
-				if (!(ast.arg3() instanceof IStringX)) {
-					throw new WrongNumberOfArguments(ast, 3, ast.argSize());
-				}
-				format = ((IStringX) ast.arg3()).toString();
-			}
-			IStringX arg1 = (IStringX) ast.arg1();
-			IExpr arg2 = ast.arg2();
-			FileWriter writer = null;
-			try {
-				writer = new FileWriter(arg1.toString());
-				if (format.equals("Table")) {
-					int[] dims = arg2.isMatrix();
-					if (dims != null) {
-						for (int j = 0; j < dims[0]; j++) {
-							IAST rowList = (IAST) arg2.getAt(j + 1);
-							for (int i = 1; i <= dims[1]; i++) {
-								if (rowList.get(i).isReal()) {
-									writer.append(rowList.get(i).toString());
-								} else {
-									writer.append("\"");
-									writer.append(rowList.get(i).toString());
-									writer.append("\"");
-								}
-								if (i < dims[1]) {
-									writer.append(" ");
-								}
-							}
-							writer.append("\n");
-						}
-						return arg1;
-					} else {
-						if (arg2.isList()) {
+    @Override
+    public IExpr evaluate(final IAST ast, EvalEngine engine) {
+        if (Config.isFileSystemEnabled(engine)) {
+            Validate.checkRange(ast, 3, 4);
 
-						}
-					}
-				} else if (format.equals("Data")) {
-					File file = new File(arg1.toString());
-					com.google.common.io.Files.write(arg2.toString(), file, Charset.defaultCharset());
-					return arg1;
-				}
+            if (!(ast.arg1() instanceof IStringX)) {
+                throw new WrongNumberOfArguments(ast, 1, ast.argSize());
+            }
+            String format = "Data";
+            if (ast.size() == 4) {
+                if (!(ast.arg3() instanceof IStringX)) {
+                    throw new WrongNumberOfArguments(ast, 3, ast.argSize());
+                }
+                format = ((IStringX) ast.arg3()).toString();
+            }
+            IStringX arg1 = (IStringX) ast.arg1();
+            IExpr arg2 = ast.arg2();
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(arg1.toString());
+                if (format.equals("Table")) {
+                    int[] dims = arg2.isMatrix();
+                    if (dims != null) {
+                        for (int j = 0; j < dims[0]; j++) {
+                            IAST rowList = (IAST) arg2.getAt(j + 1);
+                            for (int i = 1; i <= dims[1]; i++) {
+                                if (rowList.get(i).isReal()) {
+                                    writer.append(rowList.get(i).toString());
+                                } else {
+                                    writer.append("\"");
+                                    writer.append(rowList.get(i).toString());
+                                    writer.append("\"");
+                                }
+                                if (i < dims[1]) {
+                                    writer.append(" ");
+                                }
+                            }
+                            writer.append("\n");
+                        }
+                        return arg1;
+                    } else {
+                        if (arg2.isList()) {
 
-			} catch (IOException ioe) {
-				engine.printMessage("Export: file " + arg1.toString() + " not found!");
-			} finally {
-				if (writer != null) {
-					try {
-						writer.close();
-					} catch (IOException e) {
-					}
-				}
-			}
-		}
-		return F.NIL;
-	}
+                        }
+                    }
+                } else if (format.equals("Data")) {
+                    File file = new File(arg1.toString());
+                    com.google.common.io.Files.write(arg2.toString(), file, Charset.defaultCharset());
+                    return arg1;
+                }
 
-	/**
-	 * See the documentation of {@link CsvFormat}, {@link ImageFormat}, {@link MatlabExport}, and {@link ObjectFormat}
-	 * for information on how tensors are encoded in the respective format.
-	 * 
-	 * @param file
-	 *            destination
-	 * @param tensor
-	 * @throws IOException
-	 */
-	public static void of(File file, IAST tensor) throws IOException {
-		Filename filename = new Filename(file);
-		// if (filename.hasExtension("csv"))
-		// Files.write(file.toPath(), (Iterable<String>) CsvFormat.of(tensor)::iterator);
-		// else
-		Extension extension = filename.extension();
-		if (extension.equals(Extension.JPG))
-			ImageIO.write(ImageFormat.jpg(tensor), "jpg", file);
-		// else if (filename.hasExtension("m"))
-		// Files.write(file.toPath(), (Iterable<String>) MatlabExport.of(tensor)::iterator);
-		if (extension.equals(Extension.PNG))
-			ImageIO.write(ImageFormat.of(tensor), "png", file);
-		// else if (filename.hasExtension("tensor"))
-		// object(file, tensor);
-		else
-			throw new RuntimeException(file.toString());
-	}
+            } catch (IOException ioe) {
+                engine.printMessage("Export: file " + arg1.toString() + " not found!");
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
+        return F.NIL;
+    }
 }
