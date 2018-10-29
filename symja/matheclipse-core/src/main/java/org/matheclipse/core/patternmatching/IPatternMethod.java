7af26e78151452ef8c40a99de2f8d283b864fc57
duy <tranleduy1233@gmail.com>
2018-10-29 15:24:57 +0700

    error: /Users/duy/github/SymC/symc/src/main/java/org/matheclipse/core/patternmatching/IPatternMethod.java:5: FunctionalInterface cannot be resolved to a type

--- symja/matheclipse-core/src/main/java/org/matheclipse/core/patternmatching/IPatternMethod.java
+++ symja/matheclipse-core/src/main/java/org/matheclipse/core/patternmatching/IPatternMethod.java
@@ -2,7 +2,6 @@ package org.matheclipse.core.patternmatching;
 
 import org.matheclipse.core.interfaces.IExpr;
 
-@FunctionalInterface
 public interface IPatternMethod {
 	public abstract IExpr eval(PatternMap pm);
 }
