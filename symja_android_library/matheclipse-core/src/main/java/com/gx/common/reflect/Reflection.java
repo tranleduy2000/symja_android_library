17228beb127808df6013c6a09a8419259ab57ef9
duy <tranleduy1233@gmail.com>
2018-10-31 16:13:17 +0700

    Clean method Class.forName

--- symja_android_library/matheclipse-core/src/main/java/com/gx/common/reflect/Reflection.java
+++ symja_android_library/matheclipse-core/src/main/java/com/gx/common/reflect/Reflection.java
@@ -53,27 +53,6 @@ public final class Reflection {
     }
 
     /**
-     * Ensures that the given classes are initialized, as described in <a
-     * href="http://java.sun.com/docs/books/jls/third_edition/html/execution.html#12.4.2">JLS Section
-     * 12.4.2</a>.
-     * <p>
-     * <p>WARNING: Normally it's a smell if a class needs to be explicitly initialized, because static
-     * state hurts system maintainability and testability. In cases when you have no choice while
-     * inter-operating with a legacy framework, this method helps to keep the code less ugly.
-     *
-     * @throws ExceptionInInitializerError if an exception is thrown during initialization of a class
-     */
-    public static void initialize(Class<?>... classes) {
-        for (Class<?> clazz : classes) {
-            try {
-                Class.forName(clazz.getName(), true, clazz.getClassLoader());
-            } catch (ClassNotFoundException e) {
-                throw new AssertionError(e);
-            }
-        }
-    }
-
-    /**
      * Returns a proxy instance that implements {@code interfaceType} by dispatching method
      * invocations to {@code handler}. The class loader of {@code interfaceType} will be used to
      * define the proxy class. To implement multiple interfaces or specify a class loader, use {@link
