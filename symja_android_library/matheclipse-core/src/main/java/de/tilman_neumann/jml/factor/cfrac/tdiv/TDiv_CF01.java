384251c9afe3740eaa5b9fa7cccdd5eb8bd1a7dd
axexlck <axelclk@gmail.com>
2019-01-02 08:35:02 +0700

    #106 use latest jml sources
    (cherry picked from commit 4729f6c)

--- symja_android_library/matheclipse-core/src/main/java/de/tilman_neumann/jml/factor/cfrac/tdiv/TDiv_CF01.java
+++ symja_android_library/matheclipse-core/src/main/java/de/tilman_neumann/jml/factor/cfrac/tdiv/TDiv_CF01.java
@@ -15,15 +15,11 @@ package de.tilman_neumann.jml.factor.cfrac.tdiv;
 
 import java.math.BigInteger;
 
-//import org.apache.log4j.Logger;
-
 import de.tilman_neumann.jml.base.UnsignedBigInt;
 import de.tilman_neumann.jml.factor.base.SortedIntegerArray;
 import de.tilman_neumann.jml.factor.base.congruence.AQPair;
 import de.tilman_neumann.jml.factor.base.congruence.Partial_1Large;
 import de.tilman_neumann.jml.factor.base.congruence.Smooth_Perfect;
-
-import static de.tilman_neumann.jml.base.BigIntConstants.*;
 
 /**
  * Auxiliary factor algorithm to find smooth decompositions of Q's.
@@ -34,7 +30,7 @@ import static de.tilman_neumann.jml.base.BigIntConstants.*;
  * @author Tilman Neumann
  */
 public class TDiv_CF01 implements TDiv_CF {
-//	@SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 //	private static final Logger LOG = Logger.getLogger(TDiv_CF01.class);
 //	private static final boolean DEBUG = false;
 	
