b5f11cca87959c50e0cde401b2cd5f2702324d3e
duy <tranleduy1233@gmail.com>
2019-05-08 17:29:28 +0700

    Port JAS 2.6.5988 completed

--- symja_android_library/matheclipse-core/src/test/java/com/duy/CompatibleTest.java
+++ symja_android_library/matheclipse-core/src/test/java/com/duy/CompatibleTest.java
@@ -12,19 +12,21 @@ public class CompatibleTest extends TestCase {
 
 
     public void test() throws IOException {
-        String[] unavailablePackages = new String[]{
+
+        String[] unavailableSymbols = new String[]{
                 "import java.nio.channels",
                 "import java.net",
                 "import java.security",
                 ".DOUBLE_EPSILON",
                 "java.util.UUID",
         };
+
         File sourceDir = new File("../symja_android_library/matheclipse-core/src/main/java");
         assertTrue(sourceDir.exists());
         Collection<File> javaFiles = FileUtils.listFiles(sourceDir, new String[]{"java"}, true);
         for (File javaFile : javaFiles) {
             String content = FileUtils.readFileToString(javaFile);
-            for (String unavailablePackage : unavailablePackages) {
+            for (String unavailablePackage : unavailableSymbols) {
                 if (content.contains(unavailablePackage)) {
                     System.err.println("Warn: Symbol " + (unavailablePackage) + " is unavailable in class " + javaFile.getName());
                 }
