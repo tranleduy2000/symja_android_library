ea8046edb7c57e27192256d4dd85b0c1f244bd34
duy <tranleduy1233@gmail.com>
2019-05-24 20:31:31 +0700

    ComplexUtils axexlck 2019-05-19 11:50

--- symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/LowercaseTestCase.java
+++ symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/LowercaseTestCase.java
@@ -8088,8 +8088,9 @@ public class LowercaseTestCase extends AbstractTestCase {
 	public void testInverseFourier() {
 		check("InverseFourier({2.82843+I*9.19239,-1.41421+I*(-6.36396)})", //
 				"{1.0+I*2.0,3.0+I*11.0}");
+		// Swift changed: use DOUBLE_TOLERANCE
 		check("InverseFourier({1.5,0.5+I*1.0,-0.5,0.5+I*(-1.0)})", //
-				"{1.0,2.0+I*2.22045*10^-16,0.0,I*(-2.22045*10^-16)}");
+				"{1.0,2.0,0.0,0.0}");
 
 		// InverseFourier: Argument {1,2,0,0,7} is restricted to vectors with a length of power of 2.
 		check("InverseFourier({1,2,0,0,7})", //
