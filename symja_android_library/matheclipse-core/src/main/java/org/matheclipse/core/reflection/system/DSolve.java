45b21a746a693e064d04ed02791f010f57b84ac4
duy <tranleduy1233@gmail.com>
2019-06-22 20:26:07 +0700

    use F.C in DSolve axexlck 2019-06-15 21:49 Use F.C for F.CSymbol axexlck 2019-06-15 21:49

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/DSolve.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/DSolve.java
@@ -113,7 +113,7 @@ public class DSolve extends AbstractFunctionEvaluator {
 			IExpr[] boundaryCondition, EvalEngine engine) {
 				IAST listOfVariables = F.List(uFunction1Arg);
 				if (listOfEquations.size() <= 2) {
-			IExpr C_1 = F.unaryAST1(F.CSymbol, F.C1); // constant C(1)
+			IExpr C_1 = F.unaryAST1(F.C, F.C1); // constant C(1)
 					IExpr equation = listOfEquations.arg1();
 			IExpr temp = solveSingleODE(equation, xVar, listOfVariables, C_1, engine);
 					if (!temp.isPresent()) {
