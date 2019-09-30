f3d454cb66deaf2a9bd13c983e90c7ef9d6f493e
duy <tranleduy1233@gmail.com>
2019-09-30 13:59:07 +0700

    Open html file

--- symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/BasePlotExample.java
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/BasePlotExample.java
@@ -8,42 +8,57 @@ import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.parser.client.SyntaxError;
 import org.matheclipse.parser.client.math.MathException;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public abstract class BasePlotExample {
 
-	public String exampleFunction() {
-		return "Manipulate(ListPlot(Table({Sin(t), Cos(t*a)}, {t, 100})), {a,1,4,1})";
-	}
+    public String exampleFunction() {
+        return "Manipulate(ListPlot(Table({Sin(t), Cos(t*a)}, {t, 100})), {a,1,4,1})";
+    }
 
-	public void generateHTML() {
-		try {
-			ExprEvaluator util = new ExprEvaluator();
+    public void generateHTML() {
+        try {
+            ExprEvaluator util = new ExprEvaluator();
 
-			IExpr result = util.eval(exampleFunction());
-			if (result.isAST(F.JSFormData, 3)) {
-				if (result.second().toString().equals("mathcell")) {
-					String manipulateStr = ((IAST) result).arg1().toString();
-					String js = Config.MATHCELL_PAGE;
-					js = js.replaceAll("`1`", manipulateStr);
-					System.out.println(js);
-				} else {
-					String manipulateStr = ((IAST) result).arg1().toString();
-					String js = Config.JSXGRAPH_PAGE;
-					js = js.replaceAll("`1`", manipulateStr);
-					System.out.println(js);
-				}
-			}
-		} catch (SyntaxError e) {
-			// catch Symja parser errors here
-			System.out.println(e.getMessage());
-		} catch (MathException me) {
-			// catch Symja math errors here
-			System.out.println(me.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-		} catch (final StackOverflowError soe) {
-			System.out.println(soe.getMessage());
-		} catch (final OutOfMemoryError oome) {
-			System.out.println(oome.getMessage());
-		}
-	}
+            IExpr result = util.eval(exampleFunction());
+            if (result.isAST(F.JSFormData, 3)) {
+                if (result.second().toString().equals("mathcell")) {
+                    String manipulateStr = ((IAST) result).arg1().toString();
+                    String js = Config.MATHCELL_PAGE;
+                    js = js.replaceAll("`1`", manipulateStr);
+                    System.out.println(js);
+                    openHtml(js);
+                } else {
+                    String manipulateStr = ((IAST) result).arg1().toString();
+                    String js = Config.JSXGRAPH_PAGE;
+                    js = js.replaceAll("`1`", manipulateStr);
+                    openHtml(js);
+                    System.out.println(js);
+                }
+            }
+        } catch (SyntaxError e) {
+            // catch Symja parser errors here
+            System.out.println(e.getMessage());
+        } catch (MathException me) {
+            // catch Symja math errors here
+            System.out.println(me.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (final StackOverflowError soe) {
+            System.out.println(soe.getMessage());
+        } catch (final OutOfMemoryError oome) {
+            System.out.println(oome.getMessage());
+        }
+    }
+
+    private void openHtml(String content) throws IOException {
+        File htmlFile = File.createTempFile("page", ".html");
+        FileOutputStream fileOutputStream = new FileOutputStream(htmlFile);
+        fileOutputStream.write(content.getBytes());
+        fileOutputStream.close();
+
+        Runtime.getRuntime().exec("cmd /c " + htmlFile.getAbsolutePath());
+    }
 }
