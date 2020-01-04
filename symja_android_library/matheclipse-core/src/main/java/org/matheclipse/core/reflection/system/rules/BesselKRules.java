33fd1fb9e2e8c46f68a49d652843ff5a7c193434
axexlck <axelclk@gmail.com>
2020-01-04 15:20:52 +0700

    Bessel rules
    (cherry picked from commit 59d3607cd4dd1985bd1a7abe966a648a8055927b)

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/rules/BesselKRules.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/rules/BesselKRules.java
@@ -8,20 +8,20 @@ import org.matheclipse.core.interfaces.IAST;
  * <p>See GIT repository at: <a href="https://github.com/axkr/symja_android_library">github.com/axkr/symja_android_library under the tools directory</a>.</p>
  */
 public interface BesselKRules {
-    /**
-     * <ul>
-     * <li>index 0 - number of equal rules in <code>RULES</code></li>
-     * </ul>
-     */
-    final public static int[] SIZES = { 0, 2 };
+  /**
+   * <ul>
+   * <li>index 0 - number of equal rules in <code>RULES</code></li>
+	 * </ul>
+	 */
+  final public static int[] SIZES = { 0, 2 };
 
-    final public static IAST RULES = List(
-            IInit(BesselK, SIZES),
-            // BesselK(-1/2,z_):=Sqrt(2/Pi)/(E^z*Sqrt(z))
-            ISetDelayed(BesselK(CN1D2,z_),
-                    Times(Sqrt(Times(C2,Power(Pi,CN1))),Power(Times(Exp(z),Sqrt(z)),CN1))),
-            // BesselK(1/2,z_):=Sqrt(2/Pi)/(E^z*Sqrt(z))
-            ISetDelayed(BesselK(C1D2,z_),
-                    Times(Sqrt(Times(C2,Power(Pi,CN1))),Power(Times(Exp(z),Sqrt(z)),CN1)))
-    );
+  final public static IAST RULES = List(
+    IInit(BesselK, SIZES),
+    // BesselK(-1/2,z_):=Sqrt(2/Pi)/(E^z*Sqrt(z))
+    ISetDelayed(BesselK(CN1D2,z_),
+      Times(Sqrt(Times(C2,Power(Pi,CN1))),Power(Times(Exp(z),Sqrt(z)),CN1))),
+    // BesselK(1/2,z_):=Sqrt(2/Pi)/(E^z*Sqrt(z))
+    ISetDelayed(BesselK(C1D2,z_),
+      Times(Sqrt(Times(C2,Power(Pi,CN1))),Power(Times(Exp(z),Sqrt(z)),CN1)))
+  );
 }
