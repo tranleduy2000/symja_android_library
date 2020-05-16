ba9c3759e1ca945629273e4d7820de35f9199024
duy <tranleduy1233@gmail.com>
2020-05-17 01:35:22 +0700

    improve hypergeometric2F1 axexlck 1/4/20, 3:19 PM

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/generic/UnaryNumerical.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/generic/UnaryNumerical.java
@@ -5,8 +5,10 @@ import com.duy.lambda.Function;
 import org.hipparchus.analysis.UnivariateFunction;
 import org.hipparchus.analysis.differentiation.DerivativeStructure;
 import org.hipparchus.analysis.differentiation.UnivariateDifferentiableFunction;
+import org.matheclipse.core.builtin.IOFunctions;
 import org.matheclipse.core.eval.DoubleStackEvaluator;
 import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.exception.ArgumentTypeException;
 import org.matheclipse.core.expression.ComplexNum;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.expression.Num;
@@ -37,6 +39,10 @@ public class UnaryNumerical implements Function<IExpr, IExpr>, UnivariateDiffere
 	}
 
 	public UnaryNumerical(final IExpr fn, final ISymbol v, final EvalEngine engine, boolean firstDerivative) {
+		if (!v.isVariable() || v.isBuiltInSymbol()) {
+			// Cannot assign to raw object `1`.
+			throw new ArgumentTypeException(IOFunctions.getMessage("setraw", F.List(v), EvalEngine.get()));
+		}
 		fVariable = v;
 		fFunction = fn;
 		fEngine = engine;
@@ -59,6 +65,8 @@ public class UnaryNumerical implements Function<IExpr, IExpr>, UnivariateDiffere
 		try {
 			fVariable.assign(Num.valueOf(x));
 			result = DoubleStackEvaluator.eval(stack, 0, fFunction);
+		} catch (RuntimeException rex) {
+			return Double.NaN;
 		} finally {
 			fVariable.assign(value);
 		}
